0ad8a378f5c3f9c1403096b197a8c19915f34dad
shivamsy <shivamsingh.y16@iiits.in>
2018-10-17 01:45:12 +0530

    initial models made

--- Online_Rental/ors/models.py
+++ Online_Rental/ors/models.py
@@ -25,7 +25,7 @@ class UserProfile(models.Model):
 
 class LoginTrail(models.Model):
 	user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
-	time = models.DateTimeField(auto_now_add=True, blank=False)
+	timestamp = models.DateTimeField(auto_now_add=True, blank=False)
 	email = models.EmailField(max_length=60, blank=False)
 	ip = models.CharField(max_length=32)
 
@@ -54,6 +54,7 @@ class Wishlist(models.Model):
 	status = models.CharField(max_length=10, choices=(
 												('1','InStock'),('2','OutofStock')), default='1')
 	quantity = models.IntegerField(blank=False, default=1)
+	timestamp = models.DateTimeField(auto_now_add=True, blank=True)
 
 	def __str__(self):
 		return str(self.id)
@@ -62,9 +63,63 @@ class RequestSeller(models.Model):
 	buyer = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
 	seller = models.ForeignKey(UserProfile, related_name='buyer', on_delete=models.CASCADE)
 	product = models.ForeignKey(Product, on_delete=models.CASCADE)
-	datetime = models.DateTimeField(auto_now_add=True)
-	test = models.IntegerField()
+	timestamp = models.DateTimeField(auto_now_add=True, blank=True)
 
 	def __str__(self):
 		return str(self.id)
-		
+
+class ProductRating(models.Model):
+	product = models.ForeignKey(Product, on_delete=models.CASCADE)
+	buyer = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+	rating = models.DecimalField(max_digits=2, decimal_places=1, blank=False, default=1)
+	description = models.TextField(blank=True)
+	timestamp = models.DateTimeField(auto_now_add=True, blank=True)
+
+	def __str__(self):
+		return str(self.id)
+
+class SellerRating(models.Model):
+	seller = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+	buyer = models.ForeignKey(UserProfile, related_name='seller', on_delete=models.CASCADE)
+	rating = models.DecimalField(max_digits=2, decimal_places=1, blank=False, default=1)
+	description = models.TextField(blank=True)
+	timestamp = models.DateTimeField(auto_now_add=True, blank=True)
+
+	def __str__(self):
+		return str(self.id)
+
+class OrderHistory(models.Model):
+	user = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+	product = models.ForeignKey(Product, on_delete=models.CASCADE)
+	timestamp = models.DateTimeField(auto_now_add=True, blank=True)
+	dateStart = models.DateField()
+	dateEnd = models.DateField()
+
+	def __str__(self):
+		return str(self.id)
+
+class Report(models.Model):
+	complainant = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+	respondant = models.ForeignKey(UserProfile, related_name='complainant', on_delete=models.CASCADE)
+	product = models.ForeignKey(Product, on_delete=models.CASCADE)
+	timestamp = models.DateTimeField(auto_now_add=True, blank=True)
+	complain = models.TextField()
+
+	def __str__(self):
+		return str(self.id)
+
+class ArchivedProduct(models.Model):
+	owner = models.ForeignKey(UserProfile, on_delete=models.CASCADE)
+	description = models.TextField()
+	category = models.CharField(max_length=20, choices=(
+												('1','electronics'),('2','stationary'),('3','fashion'),
+												('4','sports'),('5', 'lifestyle'),('6', 'other')))
+	price = models.FloatField()
+	postdate = models.DateTimeField(auto_now_add=True, blank=False)
+	duration = models.IntegerField(blank=True)
+	quantity = models.IntegerField(blank=False, default=1)
+	ptype = models.CharField(max_length=4, choices=(
+												('1','sell'),('2','rent')), default='1')
+
+	def __str__(self):
+		return str(self.id)
