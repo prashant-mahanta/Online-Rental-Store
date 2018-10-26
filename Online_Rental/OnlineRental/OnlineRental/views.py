36e291458bd732af2a798f2bb03962af289ed6d2
PRASHANT KUMAR MAHANTA <prashantkumar.m16@iiits.in>
2018-10-27 02:51:15 +0530

    Landing Page

---
+++ Online_Rental/OnlineRental/OnlineRental/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponsePermanentRedirect
+from django.urls import reverse
+
+def direct(request):
+	return HttpResponseRedirect(reverse('ors:login'))
